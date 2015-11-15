108734ad93aa73a30680e8508618435cbf14f3ea
bisco <nobu+github@glleu.net>
2015-11-15 22:00:47 +0900

    pyoko-lang ver 0.0.1

--- 1201/brainpyoko.py
+++ 1201/brainpyoko.py
@@ -1,7 +1,8 @@
-#fineencode: utf-8
+#fineencoding: utf-8
 
 from __future__ import print_function
 import sys
+import codecs
 
 class brainpyoko():
     plus   = "+"
@@ -12,25 +13,64 @@ class brainpyoko():
     rb     = "]"
     comma  = ","
     period = "."
-    token_set = set([plus,minus,gt,lt,lb,rb,comma,period])
+    #symbol_set = set([plus,minus,gt,lt,lb,rb,comma,period])
 
     def __init__(self,filename):
-        with open(filename) as f:
+        with codecs.open(filename, "r", "utf-8") as f:
             self.raw_code = ("".join(f.readlines())).strip()
         self.iptr = 0
         self.dptr = 0
         self.data = [0]*100
         self.loopmap = {} 
         self.insts = []
-        self.parser()
+        self.__parser()
 
-    def parser(self):
-        self.tokenizer()
+    def __parser(self):
+        self.__tokenizer()
 
-    def tokenizer(self):
-        for i in self.raw_code:
-            if i in brainpyoko.token_set:
-                self.insts.append(i)
+    def __get_token(self, pos):
+        is_same = lambda raw_code, pos, x: raw_code[pos:pos+len(x)] == x
+        quasi_plus   = u"ﾋﾟｮｺ"
+        quasi_minus  = u"ﾋﾟｮｺﾘ"
+        quasi_gt     = u"ﾋﾟｮｯｺ"
+        quasi_lt     = u"ﾋﾟｮ?!"
+        quasi_lb     = u"ﾋﾟｮﾘｺ"
+        quasi_rb     = u"ﾋﾟｮｺ-"
+        quasi_comma  = u"ﾋﾟｮｺ?!"
+        quasi_period = u"ﾋﾟｮｯｺﾘﾝ"
+
+        if is_same(self.raw_code, pos, quasi_minus):
+            return quasi_minus, brainpyoko.minus
+        elif is_same(self.raw_code, pos, quasi_period):
+            return quasi_period, brainpyoko.period
+        elif is_same(self.raw_code, pos, quasi_gt):
+            return quasi_gt, brainpyoko.gt
+        elif is_same(self.raw_code, pos, quasi_lt):
+            return quasi_lt, brainpyoko.lt
+        elif is_same(self.raw_code, pos, quasi_lb):
+            return quasi_lb, brainpyoko.lb
+        elif is_same(self.raw_code, pos, quasi_rb):
+            return quasi_rb, brainpyoko.rb
+        elif is_same(self.raw_code, pos, quasi_plus):
+            return quasi_plus, brainpyoko.plus
+        elif self.raw_code[pos:pos+len(quasi_comma)] == quasi_comma:
+            return quasi_comma, brainpyoko.comma
+        else:
+            return False, False
+
+    def __tokenizer(self):
+        pos = 0
+        while pos < len(self.raw_code):
+            token, symbol = self.__get_token(pos)
+            if token is False:
+                #print(pos,end="")
+                #print(":",end="")
+                #print(self.raw_code[pos])
+                pos += 1
+                continue
+            self.insts.append(symbol)
+            pos += len(token)
+        #print("".join(self.insts))
 
     def __gen_loopmap(self):
         stack = []
@@ -88,8 +128,8 @@ def usage():
 def main():
     if len(sys.argv) != 2:
         usage()
-    b = brainpyoko(sys.argv[1])
-    b.run()
+    bp = brainpyoko(sys.argv[1])
+    bp.run()
     print()
 
 if __name__ == "__main__":
